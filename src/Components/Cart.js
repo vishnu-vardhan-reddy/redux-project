ff6822805a9816f40ddce7f2fa16a64cf82bdf0d
Vishnuvardhan Reddy <vishnuvardhan4133@gmail.com>
2021-03-26 22:43:28 +0530

    added cart

---
+++ src/Components/Cart.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import formatCurrency from "./util";
+
+export default class Cart extends Component {
+  render() {
+    const { cartItems } = this.props;
+    return (
+      <>
+        <div>
+          {cartItems.length === 0 ? (
+            <div className="cart cart-header"> Cart is empty</div>
+          ) : (
+            <div className="cart cart-header">
+              You have {cartItems.length} items in the cart
+            </div>
+          )}
+          <div className="cart">
+            <ul className="cart-items">
+              {cartItems.map((item) => (
+                <li key={item.id}>
+                  <div>
+                    <img src={item.image} alt={item.title} />
+                  </div>
+                  <div>
+                    <div>{item.title}</div>
+                    <div className="right">
+                      {formatCurrency(item.price)} x {item.count}
+                      {"   "}
+                      <button
+                        className="button"
+                        onClick={() => this.props.removeFromCart(item)}
+                      >
+                        remove
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {cartItems.length !== 0 && (
+            <div className="cart">
+              <div className="total">
+                <div>
+                  Total:{" "}
+                  {formatCurrency(
+                    cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                  )}
+                </div>
+              </div>
+              <button className="button primary">Proceed</button>
+            </div>
+          )}
+        </div>
+      </>
+    );
+  }
+}
